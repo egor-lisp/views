8621842e2b8f7c867a0a8a40ebcdce8fe8096f6b
egor-lisp <70658544+egor-lisp@users.noreply.github.com>
2021-08-11 15:07:28 +0300

    Update y.py

--- y.py
+++ y.py
@@ -30,8 +30,9 @@ def monotoring_yandex_maps(driver, urls):
 
 
 chrome_options = Options()
-#chrome_options.add_argument('--headless')
-#chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_experimental_option("prefs", {'profile.managed_default_content_settings.javascript': 2})
 driver = webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=chrome_options)
 
